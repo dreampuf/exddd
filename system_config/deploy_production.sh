1de1a8f39d5024b1fb307907c5dc9bf8f8f0c98f
Dreampuf <soddyque@gmail.com>
2015-03-15 02:33:07 +0800

    update deploy script

--- system_config/deploy_production.sh
+++ system_config/deploy_production.sh
@@ -8,10 +8,7 @@ mkdir $LOCAL_PATH
 tar -xf $PACKAGE_NAME -C $LOCAL_PATH
 cp system_config/{exddd.nginx.conf,exddd.supervisor.conf} $LOCAL_PATH
 rsync -av $LOCAL_PATH/ root@$TARGET_HOST:$TARGET_PATH/
-ssh root@$TARGET_HOST "ln -snf $TARGET_PATH/exddd.nginx.conf /etc/nginx/sites-available/exddd.nginx.conf
-ln -snf /etc/nginx/sites-available/exddd.nginx.conf /etc/nginx/sites-enabled/exddd.nginx.conf
-pkill -f web || true
-nohup /opt/exddd/run.sh > /dev/null &
-sleep 1
-exit 0
-"
+ssh root@$TARGET_HOST "ln -snf $TARGET_PATH/exddd.nginx.conf /etc/nginx/sites-available/exddd.nginx.conf"
+ssh root@$TARGET_HOST "ln -snf /etc/nginx/sites-available/exddd.nginx.conf /etc/nginx/sites-enabled/exddd.nginx.conf"
+ssh root@$TARGET_HOST "pkill -f web || true"
+ssh root@$TARGET_HOST "nohup /opt/exddd/run.sh > /dev/null &"
