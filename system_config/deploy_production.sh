a8fca5d351736eb860243f2fa07907404bc3dcb4
Dreampuf <soddyque@gmail.com>
2015-03-15 01:47:16 +0800

    Update supervisor config and circle.yml

--- system_config/deploy_production.sh
+++ system_config/deploy_production.sh
@@ -2,7 +2,15 @@
 
 PACKAGE_NAME=web/web.tar.gz
 LOCAL_PATH=local_package
+TARGET_PATH=/opt/exddd
 
 mkdir $LOCAL_PATH
 tar -xf $PACKAGE_NAME -C $LOCAL_PATH
-rsync -av $LOCAL_PATH/ root@$TARGET_HOST:/opt/exddd/
+cp system_config/{exddd.nginx.conf,exddd.supervisor.conf} $LOCAL_PATH
+rsync -av $LOCAL_PATH/ root@$TARGET_HOST:$TARGET_PATH/
+ssh root@$TARGET_HOST "ln -snf $TARGET_PATH/exddd.nginx.conf /etc/nginx/sites-available/exddd.nginx.conf
+ln -snf /etc/nginx/sites-available/exddd.nginx.conf /etc/nginx/sites-enabled/exddd.nginx.conf
+pkill -f web
+nohup /opt/exddd/run.sh > /dev/null &
+sleep 1
+"
